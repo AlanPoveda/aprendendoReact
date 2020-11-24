7741d43e71cf215cf20348cbb2e35cea366e8955
AlanPoveda <alsepoveda@gmail.com>
2020-11-24 19:58:01 -0300

    Colocando a minha primeira aplicação React no ar, aprendendo aos poucos

---
+++ firstappreact/src/App.js
@@ -0,0 +1,12 @@
+import Header from './header/index'
+import './style.css'
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
---
+++ firstappreact/src/header/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import './style.css';
+
+// Forma correta e básica para colocar aplicações sem classe
+const Header = () => (
+    <header id='main-header'>Aprendendo React 🐱‍💻</header>
+);
+
+export default Header;
